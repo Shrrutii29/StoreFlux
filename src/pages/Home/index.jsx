0acb058664d6b00087dfb42bfa5be46ba9c63115
Shruti <shrutinavale29@gmail.com>
2025-08-21 12:31:37 +0530

    feat: navbar

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components/Navbar"
+import { useEffect } from "react"
+import { getAllProducts } from "../../api/getAllProducts"
+
+export const Home = () => {
+    return (
+        <>
+            <Navbar />
+
+        </>
+    )
+}
